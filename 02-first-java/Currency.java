9978ae658d9e2c2c27891e5307c327cde255c04b
Rahul Singh <rahul.singh2301@icloud.com>
2023-12-31 19:01:00 +0530

    02-first-java-Currency

---
+++ 02-first-java/Currency.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Currency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double inr = sc.nextDouble();
+        double usd = inr * 83.19;
+
+        System.out.println(usd);
+        sc.close();
+    }
+}
