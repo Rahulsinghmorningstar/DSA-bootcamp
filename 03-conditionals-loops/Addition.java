e1f13da4c00d5b16469a23e6a6ac503744cbd099
Rahul Singh <rahul.singh2301@icloud.com>
2024-01-03 20:30:47 +0530

    03-Conditional_loops-Addition

---
+++ 03-conditionals-loops/Addition.java
@@ -0,0 +1,17 @@
+// Addition Of Two Numbers
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
